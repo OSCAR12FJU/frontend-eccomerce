be40844655eb6bcc5d486bb5db9842c6359be85c
Oscar Flores <oscar.blv.cabj@hotmail.com>
2025-02-14 15:20:18 -0300

    feat(api): add async function support in user controller

--- src/app/products/[title]/page.tsx
+++ src/app/products/[title]/page.tsx
@@ -1,7 +1,7 @@
 import ProductPage from "@/components/ProductPage";
 
 const Page = async({ params }: { params: { title: string } }) =>{
-  const resolvedParams = await params;
+  const resolvedParams = params;
   return <ProductPage title={resolvedParams.title}/>
     
 }
